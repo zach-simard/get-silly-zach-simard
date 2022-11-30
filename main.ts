588e0851d62aba1d075aa61b11809bbbf80e8c46
zachary ladouceur-simard <118993100+zach-simard@users.noreply.github.com>
2022-11-30 12:27:12 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Silly)
+})
